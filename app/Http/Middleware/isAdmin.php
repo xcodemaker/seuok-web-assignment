1bf85da93284e804663cdf752181df27b138e33a
kekayan <kekayan@ieee.org>
2018-07-18 22:13:16 +0530

    fix for /admin showing error when user not logged

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -16,9 +16,17 @@ class isAdmin
      */
     public function handle($request, Closure $next)
     {
+
         $user=Auth::user();
+        if($user){
+
+
         if(!$user->isAdmin()){
             return redirect('/');
+        }
+        }
+        else{
+            return redirect('/login');
         }
         return $next($request);
     }
