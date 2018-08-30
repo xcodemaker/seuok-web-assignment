dc0b394006f25460999f7f5ca32319cb3537856d
dhammika Saman Kumara <dhammika.dsk@gmail.com>
2018-08-31 00:10:19 +0530

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
         <!-- ********************************************************************************************* -->
 <nav class="navbar navbar-expand-sm navbar-dark bg-primary mb-3 navbar-laravel">
     <div class="container">
-  <a class="navbar-brand" href="/">Projot</a>
+  <a class="navbar-brand" >Projot</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
