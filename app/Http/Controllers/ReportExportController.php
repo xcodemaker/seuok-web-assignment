57776a74a9c213f18f2c4d595cd6a2dcc925c905
LakshanaKarunathilake <manualsplash@gmail.com>
2018-08-30 22:22:04 +0530

    ReportExportController created

--- app/Http/Controllers/ReportExportController.php
+++ app/Http/Controllers/ReportExportController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+
+
+use App\Exports\UsersExport;
+use App\Exports\ProjectsExport;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Http\Controllers\Controller;
 
 class ReportExportController extends Controller
 {
-    //
+    function excel_user(){        
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+
+    function excel_projects(){        
+        return Excel::download(new ProjectsExport, 'projects.xlsx');
+    }
 }
